b8e52cdc8fdfce4c01e0dbe0ed30778eaf9c3cfd
wakkihaya <wakkihaya@gmail.com>
2021-09-16 14:11:08 +0900

    add: scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './styles/app.scss'
 
 export const App: React.FC = () => {
-  return <>App</>
+  return (<><div className="App">App</div></>)
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
