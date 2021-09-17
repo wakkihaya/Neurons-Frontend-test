f626d7cb6d49186e8a1427650de8ca2d8a39bec1
wakkihaya <wakkihaya@gmail.com>
2021-09-17 14:53:54 +0900

    update: use paths

--- src/components/organisms/Navigation/Navigation.tsx
+++ src/components/organisms/Navigation/Navigation.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import clsx from 'clsx'
 import type { FC, ReactNode, MouseEventHandler } from 'react'
 import styles from './Navigation.scss'
-import { Button } from '../../atoms/'
+import { Button } from '~/components/atoms/'
 
 export type NavigationProps = {
   children1: ReactNode
@@ -27,7 +27,7 @@ export const Navigation: FC<NavigationProps> = (props) => {
 
   return (
     <div className={clsx(styles.navigation, className)}>
-      <img src="../../../assets/images/Logo.png" className={styles.img} />
+      <img src="~/assets/images/Logo.png" className={styles.img} />
       <Button
         onClick={onClickButton1}
         theme={themeButton1}
