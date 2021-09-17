8c28f31df1e84a89d99c1f2a89970e17d364e083
wakkihaya <wakkihaya@gmail.com>
2021-09-17 14:34:27 +0900

    update: button atoms export

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,7 +1,6 @@
 import type { FC, ReactNode, MouseEventHandler } from 'react'
 import clsx from 'clsx'
 import styles from './Button.scss'
-import React from 'react'
 
 export const ButtonTheme = {
   DEFAULT: 'default',
