d2315c441296330736dc6758b57b84169c267b1b
wakkihaya <wakkihaya@gmail.com>
2021-09-17 19:51:49 +0900

    add: fetch cast infor by api

---
+++ src/hooks/use-cast.ts
@@ -0,0 +1,53 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+import { CastModel } from '~models/CastModel'
+
+//Param: {birthday: '1982-03-10'} Return: string(e.g. '23')
+const calculateAge = (birthday: string) => {
+  if (birthday === 'None') return
+  const today = new Date()
+  const birthDate = new Date(birthday)
+  var currentAge = today.getFullYear() - birthDate.getFullYear()
+  const m = today.getMonth() - birthDate.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    currentAge--
+  }
+  return currentAge.toString()
+}
+
+export const useCast = () => {
+  const [castInfo, setCastInfo] = useState<CastModel[]>()
+
+  useEffect(() => {
+    const fetchCastByApi = async () => {
+      try {
+        const res = await axios.get(
+          'https://api.tvmaze.com/shows/143?embed=cast'
+        )
+        const resData = res.data
+
+        const castDataList = resData['_embedded']['cast']
+
+        const formattedCastDataList = castDataList.map((item: any) => {
+          const birthday = item['person']['birthday'] ?? 'None'
+          return {
+            name: item['person']['name'] ?? 'None',
+            age: calculateAge(birthday) ?? 'None',
+            country: item['person']['country']['name'],
+            birthday: birthday,
+            character: item['character']['name'],
+            charLink: item['character']['url'],
+            imageSrc: item['person']['image']['medium'],
+          } as CastModel
+        })
+        setCastInfo(formattedCastDataList)
+      } catch (error) {
+        console.error(error)
+        return
+      }
+    }
+    fetchCastByApi()
+  }, [])
+  return { castInfo }
+}
