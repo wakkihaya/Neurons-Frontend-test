11eba2cab9cd16aa5b7fadf9416ca9231086707a
wakkihaya <wakkihaya@gmail.com>
2021-09-18 20:57:53 +0900

    update: router setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Cast from './pages/Cast/Cast'
 import Episode from './pages/Episodes/Episodes'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 export const App: React.FC = () => {
   return (
     <>
-      <Episode />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Cast />
+          </Route>
+          <Route exact path="/cast">
+            <Cast />
+          </Route>
+          <Route exact path="/episodes">
+            <Episode />
+          </Route>
+        </Switch>
+      </Router>
     </>
   )
 }
